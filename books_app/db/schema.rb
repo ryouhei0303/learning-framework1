196688e9134c5ec3c37f53ec8e07a45afd10e2b7
sr20010303@icloud.com <sr20010303@icloud.com>
2019-11-15 13:21:06 +0900

    11/15 課題

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191108021508) do
+ActiveRecord::Schema.define(version: 20191115010900) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
